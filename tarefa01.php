f49b803e9aa6387f9bf8e9401bab62a4c05cb0cd
Marta Sousa <marta.sousa@gmail.com>
2019-09-29 20:32:15 -0300

    no exe5, exe3 com erro

--- tarefa01.php
+++ tarefa01.php
@@ -44,7 +44,8 @@
     echo $um." ".$dois;
     echo $dois." ".$um; */
 
-
+    
+    /*  EXERCÍCIO 03
     $variable01 = true;
     $variable02 = false;
     $variable03 = 0;
@@ -56,7 +57,7 @@
     $variable09 = 'false';
     $variable10 = null;
 
-    $meuDado = 3;
+    $meuDado = &$variable01;
 
     function tipoDado($varN)
     {
@@ -69,5 +70,38 @@
             echo 'o valor ' . $varN . ' é falso.';
         }
     }
-    tipoDado($meuDado​);
-?>
+    tipoDado($meuDado​); */
+
+    /* EXERCÍCIO 04
+    $animais = ["cachorro","gato","cavalo","porco","arara"];
+    var_dump($animais);
+    echo "<br>";
+    $animais[]="crocodilo"; //como adiciona mais de um elemento numa array?
+    $animais[]="peixe"; 
+    var_dump($animais);
+    echo "<br>";
+
+    echo "Eu gosto de animais: $animais[0], $animais[1], $animais[2], $animais[3], $animais[4], $animais[5], $animais[6]...<br>";
+
+    $animais[0]="macaco";
+    echo "Eu gosto de animais: $animais[0], $animais[1], $animais[2], $animais[3], $animais[4], $animais[5], $animais[6]...<br>";
+    
+    $animais[100]="pinguim";
+    var_dump($animais);
+
+    $animais[16]="vaca";
+    var_dump($animais);
+    echo "<br>";
+    echo "Animais: $animais[0], $animais[1], $animais[2], $animais[3], $animais[4], $animais[5], $animais[6], $animais[16], $animais[100]...<br>"; */
+   
+    
+    // EXERCÍCIO 05 - array associativo
+    $carro["marca"]= 0;
+    $carro["modelo"]= 0;
+    
+    var_dump($carro);
+
+
+    $usuarioDois = ["nome"=>"marta","sobrenome"=>"Sousa","idade"=>30];
+
+   ?>
