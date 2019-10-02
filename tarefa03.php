5e10dd5aa4b8631630a0f1dd5c214e122ca150a4
Marta Sousa <marta.sousa@gmail.com>
2019-10-02 18:20:18 -0300

    feito até o exercício 3

---
+++ tarefa03.php
@@ -0,0 +1,32 @@
+<?php 
+//EXERCÍCIO 01:
+/* 
+for ($i=1; $i <= 100; $i++) { 
+    echo $i." ";
+}
+ */
+
+//EXERCÍCIO 02:
+/* 
+$aleatorio = mt_rand(0,100);
+echo "O número gerado é $aleatorio <br>";
+
+for ($i=0; $i <= $aleatorio; $i++) { 
+    echo $i." ";
+} 
+ */
+
+//EXERCÍCIO 03 - tabuada do 2:
+/* 
+echo "Tabuáda do 2: <br>";
+
+for ($i=0; $i <= 10; $i++) { 
+    $produto = 2*$i;
+    echo "2*$i = $produto <br>";
+}
+ */
+
+//EXERCÍCIO 04
+
+
+?>
