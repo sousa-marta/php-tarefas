5470f48d001270beccbe2fb23bdc7961b9321ff5
Marta Sousa <marta.sousa@gmail.com>
2019-10-09 18:34:10 -0300

    numero do parametro pré-determinado foi colocado na mão. descobrir se existe outra forma

--- tarefa05/funcoes.php
+++ tarefa05/funcoes.php
@@ -2,10 +2,12 @@
 
 //não soube como trazer a $numeroMagico para os parâmetros
 
-// Exercício 1
+// $numeroMagico = mt_rand(1,10);
+$numeroMagico = 6;
 
-//Exercício 1a
-/* function maior ($num1, $num2, $num3){
+function maior ($num1, $num2, $num3 = 6){
+    global $funcoesExecutadas;
+    $funcoesExecutadas++;
     if ($num1 >= $num2 && $num1 >= $num3) {
         return $num1;
     } elseif ($num2 >= $num3 && $num2 >= $num1 ) {
@@ -13,34 +15,23 @@
     } else {
         return $num3;
     }
-} */
+}
 
-
-$numeroMagico = mt_rand(1,10);
-
-function tabela ($first, $numeroMagico) {
-    global $funcoesExecutadas++;
+function tabela ($first, $last = 6) {
+    global $funcoesExecutadas;
+    $funcoesExecutadas++;
     $sequencia = [];
-    for ($i=0; $i <= $numeroMagico; $i++) { 
+    for ($i=$first; $i <= $last; $i++) { 
         $sequencia[]=$i;
     }
     return $sequencia;
 }
 
-function maior ($num1, $num2, $numeroMagico){
-    global $funcoesExecutadas++;
-    if ($num1 >= $num2 && $num1 >= $numeroMagico) {
-        return $num1;
-    } elseif ($num2 >= $numeroMagico && $num2 >= $num1 ) {
-        return $num2;
-    } else {
-        return $numeroMagico;
-    }
-}
+// $array = tabela(9);
+// var_dump ($array);
 
-$array = maior(1,3,3);
 
-var_dump ($array);
+// echo maior(2,2);
 
 
 
--- tarefa05/tudoJunto.php
+++ tarefa05/tudoJunto.php
@@ -1,12 +1,15 @@
 <?php 
 // Exercício 4
 
+//Contagem de quantas vezes foram executadas as funções
 $funcoesExecutadas = 0;
 
 include("funcoes.php");
 
-tabela (3,4);
-tabela (1,4);
+tabela (3,10);
+maior (5,3,29);
+tabela (4);
 
 echo $funcoesExecutadas;
+
 ?>
