36d57d8eddbcb9450dc30d6bc48a966813f8f9cd
Marta Sousa <marta.sousa@gmail.com>
2019-10-17 16:02:55 -0300

    exercício 1 continua com problema

--- tarefa08/uploads.php
+++ tarefa08/uploads.php
@@ -1,3 +1,29 @@
+<?php
+// EXERCÍCIO 1A:
+
+//Dúvida: a pasta uploads já tinha que ter sido criada?
+//Continua não funcionando
+
+if($_POST){
+	$imgName = $_FILES['sentFile']['name'];
+	$tmpLocation = $_FILES['sentFile']['tmp_name'];
+	$saveTo = 'uploads/'.$imgName;
+	
+	
+	// var_dump($_FILES);
+	var_dump($_POST);
+	
+	$ok = move_uploaded_file($tmpLocation,$saveTo);
+	echo "Deu certo!";
+	
+	if($_FILES["sentFile"]["error"] == UPLOAD_ERR_OK) {
+	}else {
+		echo "Já há um arquivo com esse nome, favor enviar outro arquivo.";
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -13,27 +39,5 @@
 	</form>
 </body>
 </html>
-
-
-<?php
-// EXERCÍCIO 1A:
-
-//ainda não está funcionando =(
-
-if($_POST){
-	$imgName = $_FILES["sentFile"]["name"];
-	$tmpLocation = $_FILES["sentFile"]["tmp_name"];
-	$saveTo = "uploads/".$imgName;
-
-	if($_FILES["sentFile"]["error"] == UPLOAD_ERR_OK) {
-		$ok = move_uploaded_file($tmpLocation,$saveTo);
-		echo "Deu certo!";
-	}else {
-		echo "Já há um arquivo com esse nome, favor enviar outro arquivo.";
-	}
-}
-
-
-?>
 
 <br><br><br>
