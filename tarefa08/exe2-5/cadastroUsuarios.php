14b4c5e74277017e480f2859fe76b5ffd3572386
Marta Sousa <marta.sousa@gmail.com>
2019-10-21 18:54:49 -0300

    criei tabela de usuários cadastrados

--- tarefa08/exe2-5/cadastroUsuarios.php
+++ tarefa08/exe2-5/cadastroUsuarios.php
@@ -1,4 +1,7 @@
 <?php
+
+  $users = json_decode(file_get_contents(__DIR__."/usuarios.json"),true); 
+
   function cadastrarUsuario($name,$age,$img,$cv){
     $fileName = "usuarios.json";
 
@@ -13,9 +16,9 @@
       $write = file_put_contents($fileName,$json); //salva em config json
 
       if($write) {
-        echo "O usuário foi adicionado corretamente";
+        return "O usuário foi adicionado corretamente";
       }else {
-        echo "Houve um problema ao adicionar o usuário";
+        return "Houve um problema ao adicionar o usuário";
       }
 
     //Se ainda não existir arquivo .json, cria:
@@ -28,9 +31,9 @@
       $write = file_put_contents($fileName,$json); //salva em config json
 
       if($write) {
-        echo "O usuário foi adicionado corretamente";
+        return "O usuário foi adicionado corretamente";
       }else {
-        echo "Houve um problema ao adicionar o usuário";
+        return "Houve um problema ao adicionar o usuário";
       }
 
     }
@@ -54,7 +57,7 @@
 
     $saveCV = move_uploaded_file($cvTmpPath,$cvFinalPath);
 
-    //Imprimindo na tela 
+    //Imprime na tela se deu certo ou não (imprime os returns de dentro da função)
     echo cadastrarUsuario($_POST["userName"],$_POST["userAge"],$imgFinalPath,$cvFinalPath);
   }
   
@@ -71,31 +74,63 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-  <main class="container d-flex justify-content-center p-5">
-    <div class="row">
-      <div class="col-12">
-        <h4>Cadastro do Usuário</h4>
+  <main class="container">
+    <!-- Formulário de Input de Dados: -->
+    <section class="d-flex justify-content-center p-5">
+      <div class="row">
+        <div class="col-12">
+          <h4>Cadastro do Usuário</h4>
+        </div>
+        <div class="col-12">
+          <form action="" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+              <input type="text" class ="form-control" name="userName" placeholder="Nome do Usuário">
+            </div>
+            <div class="form-group">
+              <input type="number" class ="form-control" name="userAge" placeholder="Idade">
+            </div>
+            <div class="form-group">
+              <label>Imagem de Perfil:</label>
+              <input type="file" name="userImage">
+            </div>
+            <div class="form-group">
+              <label>Carregue seu CV:</label>
+              <input type="file" name="userCV">
+            </div>
+            <button type="submit" class="btn btn-info">Cadastrar Usuário</button>
+          </form>
+        </div>
       </div>
+    </section>
+
+    <!-- Tabela de Usuários Cadastrados: -->
+    <section class="row d-flex justify-content-around">
       <div class="col-12">
-        <form action="" method="post" enctype="multipart/form-data">
-          <div class="form-group">
-            <input type="text" class ="form-control" name="userName" placeholder="Nome do Usuário">
-          </div>
-          <div class="form-group">
-            <input type="number" class ="form-control" name="userAge" placeholder="Idade">
-          </div>
-          <div class="form-group">
-            <label>Imagem de Perfil:</label>
-            <input type="file" name="userImage">
-          </div>
-          <div class="form-group">
-            <label>Carregue seu CV:</label>
-            <input type="file" name="userCV">
-          </div>
-          <button type="submit" class="btn btn-info">Cadastrar Usuário</button>
-        </form>
+        <h2>Todos os Usuários Cadastrados</h2>
+        <?php if($users){ ?>
+          <table>
+            <tr>
+              <th>Nome</th>
+              <th>Idade</th>
+              <th>Imagem</th>
+              <th>CV</th>
+            </tr>
+            <?php foreach($users as $row){ ?>
+            <tr>
+              <td><?= $row["userName"]; ?></td>
+              <td><?= $row["userAge"]; ?></td>
+              <td><?= $row["userImage"]; ?></td>
+              <td><?= $row["userCV"]; ?></td>
+            </tr>
+            <?php } ?>
+          </table> 
+        <?php } ?>
       </div>
-    </div>
+    </section>
+
+
+
+
 </main>
 </body>
 </html>
---
+++ tarefa08/exe2-5/perfil.php
@@ -0,0 +1,3 @@
+4. Criar um arquivo ​ perfil.php​ que ​ exibe e permite ​ editar as informações de cada um dos usuários. Para isso, é
+necessário acessar por meio da URL ​ perfil.php?id=8​ (que neste caso é um exemplo onde pedimos para mostrar informações do usuário com o id 8)
+5. Adicionar a ​ perfil.php​ as possibilidades baixar os arquivos Imagem de perfil e CV
